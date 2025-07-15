8ec08127dd816504344053ac7305bb6c86162aa6
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-07-15 23:06:26 +0530

    Intersection of Two Arrays - Brute Force

--- intersection.cpp
+++ intersection.cpp
@@ -1,36 +1,40 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void intersection(int arr1[], int size1, int arr2[], int size2)
-{
-	for(int i=0;i<size1;i++)
-	{
-		for(int j=0;j<size2;j++)
-		{
-			if(arr1[i]==arr2[j])
-			cout<<arr2[j]<<" ";
-		}
-	}
+vector<int> intersection(int arr1[], int arr2[], int n1, int n2){
+    vector<int> ans;
+    int vis[n2]={0};
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n2;j++)
+        {
+            if(arr1[i]==arr2[j] && vis[j]==0)
+            {
+                ans.push_back(arr1[i]);
+                vis[j]=1;
+                break;
+            }
+            if(arr2[j]>arr1[i]) break;
+        }
+    }
+
+    return ans;
 }
+
 int main(){
-	int size1,size2;
+    int n1,n2;
+    cout<<"Enter the size of array first array : ";
+    cin>>n1;
+    int arr1[n1];
+    cout<<"Enter the vals in array first array: : ";
+    for(int i=0;i<n1;i++) cin>>arr1[i];
 
-	cout<<"Enter the size of the first array :";
-	cin>>size1;
+    cout<<"Enter the size of array second array : ";
+    cin>>n2;
+    int arr2[n2];
+    cout<<"Enter the vals in array second array: : ";
+    for(int i=0;i<n2;i++) cin>>arr2[i];
 
-	int arr1[size1];
-	cout<<"Enter the elements in first array :";
-	for(int i=0;i<size1;i++)
-	cin>>arr1[i];
-	
-	cout<<"Enter the size of the second array :";
-	cin>>size2;
-
-	int arr2[size2];
-	cout<<"Enter the elements in second array :";
-	for(int i=0;i<size2;i++)
-	cin>>arr2[i];
-
-	cout<<"The intersection of arrays are/is : ";
-	intersection(arr1,size1,arr2,size2);
+    vector<int> ans = intersection(arr1,arr2,n1,n2);
+    for(auto itr: ans) cout<<itr<<" ";
 }
