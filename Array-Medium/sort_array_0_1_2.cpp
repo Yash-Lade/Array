5292553f15ffeb754cf1c749a2b2a6a0638beb9f
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-07-25 22:57:21 +0530

    Sort an array of 0s, 1s, and 2s - Better

---
+++ Array-Medium/sort_array_0_1_2.cpp
@@ -0,0 +1,17 @@
+void sortColors(vector<int>& nums) {
+        int cnt1=0,cnt2=0,cnt0=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) cnt0++;
+            else if(nums[i]==1) cnt1++;
+            else cnt2++;
+        }
+        for(int i=0;i<cnt0;i++){
+            nums[i]=0;
+        }
+        for(int i=cnt0;i<cnt0+cnt1;i++)
+        nums[i]=1;
+
+        for(int i=cnt0+cnt1;i<nums.size();i++)
+        nums[i]=2;
+    }
