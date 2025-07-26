85e6190ffba2457730d14b79d78f57e00fd0146f
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-07-26 18:56:33 +0530

    Sort an array of 0s, 1s, and 2s - Better

--- Array-Medium/sort_array_0_1_2.cpp
+++ Array-Medium/sort_array_0_1_2.cpp
@@ -1,17 +1,37 @@
-void sortColors(vector<int>& nums) {
-        int cnt1=0,cnt2=0,cnt0=0;
+#include<iostream>
+#include<vector>
+using namespace std;
 
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) cnt0++;
-            else if(nums[i]==1) cnt1++;
-            else cnt2++;
-        }
-        for(int i=0;i<cnt0;i++){
-            nums[i]=0;
-        }
-        for(int i=cnt0;i<cnt0+cnt1;i++)
-        nums[i]=1;
+void sortColors(int arr[], int n){
+    int cnt0=0,cnt1=0,cnt2=0;
 
-        for(int i=cnt0+cnt1;i<nums.size();i++)
-        nums[i]=2;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0) cnt0++;
+        else if(arr[i]==1) cnt1++;
+        else cnt2++;
     }
+
+    for(int i=0;i<cnt0;i++)
+    arr[i]=0;
+
+    for(int i=cnt0;i<cnt0+cnt1;i++)
+    arr[i]=1;
+
+    for(int i=cnt0+cnt1;i<n;i++)
+    arr[i]=2;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the elements: ";
+    for(int i=0;i<n;i++) cin>>arr[i];
+
+    sortColors(arr,n);
+    cout<<"After sorting array: ";
+    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+
+}
